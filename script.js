e301bfd36ee8ef1b536dec3d6b4a5437a579c846
Lucas576-ste <lucasmenezes4741@gmail.com>
2025-10-14 17:45:11 -0300

    usando >= no sorteador

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ function generateNumber(){
     const primeiro = Math.ceil(document.querySelector('.primeiro-input').value) // 
     const segundo = Math.floor(document.querySelector('.segundo-input').value)
 
-    const result = Math.floor(Math.random() * (segundo - primeiro + 1)) + primeiro;
+    if(primeiro >= segundo){
+        alert('este número precisa ser menor')
 
+    }
+    else{
+    const result = Math.floor(Math.random() * (segundo - primeiro + 1)) + primeiro;
     alert(result)
+
+
+    }
+
+
 
 
 } 
